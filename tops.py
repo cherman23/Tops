075a52b44718d61cbcc299311313ade2008cadf4
Carter Herman <carter@jce5.com>
2024-01-29 10:30:15 -0500

    Added ability to choose scope

--- tops.py
+++ tops.py
@@ -9,26 +9,36 @@ SPOTIPY_CLIENT_ID = ENV_VARS['SPOTIPY_CLIENT_ID']
 SPOTIPY_CLIENT_SECRET = ENV_VARS['SPOTIPY_CLIENT_SECRET']
 REDIRECT_URI = ENV_VARS['REDIRECT_URI']
 
-def print_User_Top_Artists():
-    results = sp.current_user_top_artists(offset=0, time_range='long_term')
+def print_User_Top_Artists(term):
+    results = sp.current_user_top_artists(offset=0, time_range=term)
     for idx, artist in enumerate(results['items']):
         artist_name = artist['name']
         print(f"{idx+1}: {artist_name}")
 
-def print_User_Top_Tracks():
-    tracks = sp.current_user_top_tracks(offset=0, time_range='short_term')
+def print_User_Top_Tracks(term):
+    tracks = sp.current_user_top_tracks(offset=0, time_range=term)
     for idx, track in enumerate(tracks['items']):
         track_name = track['name']
         print(f"{idx+1}: {track_name}")
+
+def getTerm():
+    term = input("Would you prefer short, medium, or long term results? (S , M, or L): ")
+    if term.lower() == "s": return 'short_term'
+    elif term.lower() == "m": return 'medium_term'
+    elif term.lower() == "l": return 'long_term'
+    else:
+        print("Please enter a valid response.")
+        getTerm()
 
 def main():
     Text = input("Would you like to see your top tracks or artists? (T or A): ")
     Artists = "a"
     Tracks = "t"
+    term = getTerm()
     if Text.lower() == Tracks:
-        print_User_Top_Tracks()
+        print_User_Top_Tracks(term)
     elif  Text.lower() == Artists:
-        print_User_Top_Artists()
+        print_User_Top_Artists(term)
     else:
         print("Please enter a valid response.")
         main()
